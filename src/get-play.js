d66d128d37f59975d7bd57ee5cfd3f9f46821ea5
Phil Fitch <mostreluctantly@gmail.com>
2019-07-18 11:43:30 -0700

    get-play tests pass

--- src/get-play.js
+++ src/get-play.js
@@ -1,7 +1,20 @@
-function getPlayFromNumber() {
 
+export default function getPlay() {
+    const randomNumber = Math.random();
+
+    const play = getPlayFromNumber(randomNumber);
+
+    return play;
 }
 
-function getPlay() {
-    
+export function getPlayFromNumber(randomNumber) {
+    if(randomNumber < (1 / 3)) {
+        return 0;
+    }
+    else if(randomNumber >= (2 / 3)) {
+        return 2;
+    }
+    else {
+        return 1;
+    }
 }
