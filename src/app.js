0b1321b48e7c2eb8729dde23931bd0b7e72af610
Joe Klause <joeklause@gmail.com>
2019-07-19 09:51:59 -0700

    refactor guess and app js

--- src/app.js
+++ src/app.js
@@ -8,52 +8,45 @@ quizButton.onclick = function() {
 
     alert('Welcome to the quiz!');
 
-    let name = '';
 
     const firstName = prompt('First name:');
-    name += firstName;
-
     const lastName = prompt('Last name:');
-    name += ' ' + lastName;
+    const name = firstName + ' ' + lastName;
 
     const confirmed = confirm('Are you sure you\'re ready?');
     if(confirmed === false) {
         return;
     }
 
+
     let correctAnswers = 0;
 
     const isFamilyMan = prompt('Do I have kids?');
-
     const correctIsFamilyMan = true;
     if(isYes(isFamilyMan) === correctIsFamilyMan) {
         correctAnswers += 1;
     } 
 
     const homework = prompt('Am I caught up on my homework?');
-
     const correctHomework = false;
     if(isYes(homework) === correctHomework) {
         correctAnswers += 1;
     }
 
     const isSleeping = prompt('Is my favorite activity sleeping?');
-    
     const correctIsSleeping = true;
     if(isYes(isSleeping) === correctIsSleeping) {
         correctAnswers += 1;
     }
 
+
     alert('Check your results below!');
 
-    let response = 'Well, ' + name + ', ';
-
     if(correctAnswers === 3) {
-        response += 'you got three of three! You know me so well.';
+        const response = 'Well, ' + name + ', you got three of three! You know me so well.';
+        result.textContent = response;
     } else {
-        response += 'you oughta read more closely! Try again!';
+        const response = 'Well, ' + name + ', you oughta read more closely! Try again!';
+        result.textContent = response;
     }
-
-    result.textContent = response;
-
 };
